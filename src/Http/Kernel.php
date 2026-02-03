918e47446859b4f6556d16b4cd902874a9e0cd9f
pyaephyokhant760 <pyaephyokhant2020dc@gmail.com>
2026-02-03 14:02:55 +0630

    oop

--- src/Http/Kernel.php
+++ src/Http/Kernel.php
@@ -2,7 +2,26 @@
 
 namespace PixelFix\Framework\Http;
 
+use FastRoute\RouteCollector;
+
+use function FastRoute\simpleDispatcher;
+
 class Kernel
 {
-
+    public function handle(Request $request): Response
+    {
+        $dispatcher = simpleDispatcher(function (RouteCollector $routeCollector) {
+            $routeCollector->addRoute("GET", '/', function () {
+                $content = "<h1>Hello, World!</h1>";
+                return new Response($content);
+            });
+            $routeCollector->addRoute("GET", '/books/{id:\d+}', function ($id) {
+                $content = "<h1>This is book number $id</h1>";
+                return new Response($content);
+            });
+        });
+        $routeInfo = $dispatcher->dispatch($request->getMethod(), $request->getUri());
+        [$status , $handler , $vars] = $routeInfo;
+        return call_user_func_array($handler,$vars);
+    }
 }
